a6d5d65a990e04d82eae7782d6b1d0eab425921e
ianmicah35 <ianmicah35@gmail.com>
2020-11-03 14:37:48 -0500

    first commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,8 +5,9 @@ const Form = (props) => {
     // const [firstName, setFName] = useState("");
     // const [lastName, setLName] = useState("");
     // const [email, setEmail] = useState("");
-    // const [Password, setPassword] = useState("");
+    // const [password, setPassword] = useState("");
     // const [confirmPW, setConfPW] = useState("");
+    // const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
 
     // Another way to establish state using a key:value; all form inputs in one object
     const [form, setForm] = useState({
@@ -14,8 +15,29 @@ const Form = (props) => {
         lastName: "",
         email: "",
         password: "",
-        confPW: ""
+        confPW: "",
+        HasBeenSubmitted: false
     });
+
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = form;
+        console.log("Welcome", newUser);
+        setForm({
+            // spread form adds everything that was already in the form
+            ...form,
+            // as well as what was changed
+            HasBeenSubmitted: true
+        });
+    };
+
+    const formMessage = () => {
+        if( form.HasBeenSubmitted ) {
+            return "Thank you for submitting the form!";
+        } else {
+            return "Welcome, please submit the form";
+        }
+    };
 
     const onChangeHandler = (e) => {
         setForm({
@@ -24,25 +46,25 @@ const Form = (props) => {
             // as well as what was changed
             [e.target.name]: e.target.value
         });
-    }
+    };
 
     const nameValidation = (nameInput) => {
-        if(nameInput.length >= 1)
+        if(nameInput.length >= 2)
             return true;
         return false;
-    }
+    };
 
     const emailValidation = (emailInput) => {
-        if(emailInput.length >= 7)
+        if(emailInput.length >= 5)
             return true;
         return false;
-    }
+    };
 
     const PWValidation = (passwordInput) => {
         if(passwordInput.length >= 8)
             return true;
         return false;
-    }
+    };
 
     const allValid = (formInput) => {
         return nameValidation(formInput.firstName)
@@ -50,20 +72,19 @@ const Form = (props) => {
         && emailValidation(formInput.email)
         && PWValidation(formInput.password)
         && PWValidation(formInput.confPW);
-    }
+    };
 
     return(
-        <div>
-            <h1>Please complete the form below</h1>
+        <div className="container">
 
-            <form>
-                {/* </form> className="col-5"> */}
-                <div className="form-group">
+            <form onSubmit={ createUser }>
+                <h3>{ formMessage() }</h3>
+                    <div className="form-group">
                     <label>First Name:</label>
                     {/* <input className="form-control" name="firstName" onChange={(e) => setFName(e.target.value)}></input> */}
                     <input className="form-control" name="firstName" onChange={onChangeHandler}></input>
                     {/* nameValidation, if the first part is false and the second part is true */}
-                    {!nameValidation(form.firstName) && <p className="alert alert-danger" small>Must enter at least one character.</p>}
+                    <small id="passwordHelpBlock" >{!nameValidation(form.firstName) && <p className="alert alert-danger" small>Field must be at least two characters.</p>}</small>
 
                 </div>
                 <div className="form-group">
@@ -71,44 +92,43 @@ const Form = (props) => {
                     {/* <input className="form-control" name="lastName" onChange={(e) => setLName(e.target.value)}></input> */}
                     <input className="form-control" name="lastName" onChange={onChangeHandler}></input>
                     {/* nameValidation using ternary operator */}
-                    {nameValidation(form.lastName) ? "" : <p className="alert alert-danger">Must enter at least one character</p>}
+                    <small id="passwordHelpBlock">{nameValidation(form.lastName) ? "" : <p className="alert alert-danger">Field must be at least two characters.</p>}</small>
                 </div>
-
                 <div className="form-group">
                 <label>Email:</label>
                     {/* <input className="form-control" name="email" onChange={(e) => setEmail(e.target.value)}></input> */}
                     <input className="form-control" name="email" onChange={onChangeHandler}></input>
-                    {!emailValidation(form.email) && <p className="alert alert-danger">Invalid input</p>}
+                    <small id="passwordHelpBlock">{!emailValidation(form.email) && <p className="alert alert-danger">Field must be at least five characters.</p>}</small>
                 </div>
 
                 <div className="form-group">
                 <label>Password:</label>
                     {/* <input className="form-control" name="password" onChange={(e) => setPassword(e.target.value)} v></input> */}
                     <input type="password" className="form-control" name="password" onChange={onChangeHandler}></input>
-                    {!PWValidation(form.password) && <p className="alert alert-danger">Password must be at least eight characters</p>}
+                    <small id="passwordHelpBlock">{!PWValidation(form.password) && <p className="alert alert-danger">Password must be at least eight characters</p>}</small>
                 </div>
 
                 <div className="form-group">
                 <label>Confirm Password:</label>
                     {/* <input className="form-control" name="confPW" onChange={(e) => setConfPW(e.target.value)} v></input> */}
                     <input type="password" className="form-control" name="confPW" onChange={onChangeHandler}></input>
-                    {!PWValidation(form.confPW) && <p className="alert alert-danger">Must match password above</p>}
+                    <small id="passwordHelpBlock">{!PWValidation(form.confPW) && <p className="alert alert-danger">Invalid entry</p>}</small>
                 </div>
 
                 {allValid(form) ? <input type="submit" className="btn btn-primary"/> : <input type="submit" className="btn btn-danger" disabled/>}
                 {/* <input type="submit" className="btn btn-primary" disabled/> */}
             </form>
 
-            <div>
+            {/* <div>
                 <h2>Live Form State</h2>
                 <p>First Name: {form.firstName}</p>
                 <p>Last Name: {form.lastName}</p>
                 <p>Email: {form.email}</p>
-                <p>Password:  {form.password} </p>
+                <p>Password: {form.password} </p>
                 <p>Confirm Password: {form.confPW}</p>
-            </div>
+            </div> */}
         </div>
-    )
+    );
 }
 
 export default Form;
